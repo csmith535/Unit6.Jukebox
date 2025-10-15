28582f59b7935c9062a827e0023b44849d226f37
Cameron Smith <cameron.smith3030@gmail.com>
2025-10-14 22:15:41 -0400

    post /playlists route

--- api/playlists.js
+++ api/playlists.js
@@ -1,13 +1,35 @@
 import express from "express";
-import { getPlaylists, getPlaylistById } from "../db/queries/playlists.js";
+import {
+  getPlaylists,
+  getPlaylistById,
+  createPlaylist,
+} from "../db/queries/playlists.js";
 
 const router = express.Router();
 export default router;
 
-router.route("/").get(async (req, res) => {
-  const playlists = await getPlaylists();
-  res.status(200).send(playlists);
-});
+router
+  .route("/")
+  .get(async (req, res) => {
+    const playlists = await getPlaylists();
+    res.status(200).send(playlists);
+  })
+  .post(async (req, res) => {
+    if (!req.body) return res.status(400).send("Request body required.");
+
+    const { name, description } = req.body;
+    if (!name || !description) {
+      return res.status(400).send("Request body requires Name and Description");
+    }
+
+    const newPlaylist = await createPlaylist(name, description);
+    res.status(201).send(newPlaylist);
+  });
+
+router
+  .route("/:id/tracks")
+  .get(async (req, res) => {})
+  .post(async (req, res) => {});
 
 router.route("/:id").get(async (req, res) => {
   const { id } = req.params;
